e088f33967ec8c79a66a13130054c561aa9fdf58
Jack Walker <whoisjackwalker@gmail.com>
2025-08-09 18:24:26 +0100

    initial commit

---
+++ recipesMain/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RecipesmainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'recipesMain'
---
+++ recipesMain/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class recipe(models.Model):
+    title = models.CharField(max_length = 50)
+    body = models.TextField()
+    image = models.ImageField()
+    tag = models.CharField()
+
+    def __str__(self):
+        return self.title
+
+class category(models.Model):
+    categoryTitle = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.categoryTitle
+        
+class image(models.Model):
+    imageTitle = models.CharField(max_length = 30)
+    recipeImage = models.ImageField()
+
+    def __str__(self):
+        return self.imageTitle
