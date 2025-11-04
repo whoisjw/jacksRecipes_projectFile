9198caa9a7fcffa809f3a18bce6316aa33ff5b83
Jack Walker <whoisjackwalker@gmail.com>
2025-11-04 14:44:35 +0000

    fixed formatting on exanded images and submission form

--- recipesMain/forms.py
+++ recipesMain/forms.py
@@ -7,8 +7,8 @@ class addRecipe(ModelForm):
         model = recipe
         widgets = {
             "title": Textarea(attrs={"placeholder":"Enter a fitting title...", "rows":1, "cols": 32}),
-            "ingredients": Textarea(attrs={"placeholder":"Enter your recipe's ingredients here...", "rows":10, "cols":64}),
-            "body": Textarea(attrs={"placeholder":"Enter your instructions here...", "rows":20, "cols": 64})
+            "ingredients": Textarea(attrs={"placeholder":"Enter your recipe's ingredients here...", "rows":10, "cols":128}),
+            "body": Textarea(attrs={"placeholder":"Enter your instructions here...", "rows":15, "cols": 128})
         }
         fields = ["title",
                 "image",
