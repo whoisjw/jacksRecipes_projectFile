b7487a28c44d5efa670095fb593af765d14cdb93
Jack Walker <whoisjackwalker@gmail.com>
2025-11-13 11:13:23 +0000

    db migration

--- recipesMain/models.py
+++ recipesMain/models.py
@@ -13,8 +13,8 @@ class category(models.Model):
 class recipe(models.Model):
     recipeId = models.BigAutoField(primary_key = True)
     title = models.CharField(max_length = 50)
-    prepTime = models.IntegerField()
-    cookTime = models.IntegerField()
+    prepTime = models.IntegerField(default=15)
+    cookTime = models.IntegerField(default=20)
     ingredients = models.TextField()
     body = models.TextField()
     image = models.ImageField(upload_to="food_images/", default="food_images/icon.png")
